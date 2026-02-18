2431bf9afeab00394c2e70efea179ab6ec329a0d
andreyDanilenko <danilenko.a.g@mail.ru>
2026-02-18 17:20:06 +0300

    add(store): add configure store

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,4 @@
+export { default as BaseButton } from './BaseButton/BaseButton';
+export { default as InputGroup } from './InputGroup/InputGroup';
+export { default as LogoBadge } from './LogoBadge/LogoBadge';
+export * from './icon';
